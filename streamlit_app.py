e29eb51e1653362c4bc9826e88be7b4a92e0d835
Min Kyaw Nyunt <minkn@hanamicrofinance.com>
2022-06-11 16:01:50 +0630

    gg

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,10 @@ import pandas as pd
 import numpy as np
 
 
-st.title('Uber pickups in NYC')
+st.title('Greetings')
+form = st.form(key='my_form')
+name = form.text_input(label='Enter Your Name')
+submit_button = form.form_submit_button(label='Submit')
+
+if submit_button:
+    st.write(f'Hello {name}')
